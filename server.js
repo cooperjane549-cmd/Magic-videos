c024a2405959f1dda4ef86e1d7c04acac120589f
cooperjane549-cmd <cooperjane549@gmail.com>
2026-08-22 08:44:28 +0300

    Update Firebase database URL in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://YOUR_FIREBASE_PROJECT_ID-default-rtdb.firebaseio.com"
+    databaseURL: "https://magic-tortoise-default-rtdb.firebaseio.com"
 });
 
 const db = admin.database();
